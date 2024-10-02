1dbe0ad3ad4c12dfe04dcbcb9bc4b288fa7ea2c4
emilym1989 <emilymorrow1989@gmail.com>
2024-10-02 14:20:54 -0700

    pokemon list
    added pokemon list on js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,29 @@
-
+let pokemonList = [
+    {
+        name: 'Wigglytuff',
+        speed: 45,
+        weight: '12kg',
+        types: ['cute-charm', 'frisk'],
+    }
+    {
+        name: 'Arcanine',
+        speed: 95,
+        weight: '155kg',
+        types: ['flash-fire', 'intimidate', 'justified'],
+    }
+    {
+        name: 'Noctowl',
+        speed: 70,
+        weight: '40.8kg',
+        types: ['insomnia', 'keen-eye', 'tinted-lens'],
+    }
+    {
+        name: 'Cinccino',
+        speed: 115,
+        weight: '7.5kg',
+        types: ['cute-charm', 'skill-link', 'technician'],
+    }
+]
 
 
 
