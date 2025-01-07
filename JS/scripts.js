795d642b5c4f87649e74536660c3dda77e10ce8f
emilym1989 <emilymorrow1989@gmail.com>
2025-01-07 12:26:01 -0800

    Update scripts.js
    iterated over pokemon list, write names with weights and determine chonkiness based on weight

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,19 +4,19 @@ let pokemonList = [
         speed: 45,
         weight: '12kg',
         types: ['cute-charm', 'frisk'],
-    }
+    },
     {
         name: 'Arcanine',
         speed: 95,
         weight: '155kg',
         types: ['flash-fire', 'intimidate', 'justified'],
-    }
+    },
     {
         name: 'Noctowl',
         speed: 70,
         weight: '40.8kg',
         types: ['insomnia', 'keen-eye', 'tinted-lens'],
-    }
+    },
     {
         name: 'Cinccino',
         speed: 115,
@@ -24,6 +24,26 @@ let pokemonList = [
         types: ['cute-charm', 'skill-link', 'technician'],
     }
 ]
+
+//iterate over the pokemonList and write Pokemon names with weights
+
+for (let i = 0; i < pokemonList.length; i++) {
+    // extract the numeric value of weight (remove 'kg' and convert to a number)
+    let weight = parseFloat(pokemonList[i].weight);
+
+    // determine chonkiness based on weight
+
+    let chonkiness;
+    if (weight <=10) {
+        chonkiness = "not a chonkster";
+    } else if (weight > 10 && weight <= 50) {
+        chonkiness = "moderately chonky";
+    } else{
+        chonkiness = "chonk monster";
+    }
+    
+    document.write(`<p>${pokemonList[i].name} (weight: ${pokemonList[i].weight}) - ${chonkiness}</p>`);
+}
 
 
 
